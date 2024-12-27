879214de4024a474af4c300e66330c222b1ed03f
Ben Herman <benherman345@gmail.com>
2024-12-26 20:18:21 -0800

    address change

--- src/backend/address.h
+++ src/backend/address.h
@@ -2,17 +2,19 @@
 #define address_h
 
 #include <vector>
+
 #include "backend/defs.h"
 
 class Address {
-private:
-    std::vector<block_id_t> address;
-
 public:
-    Address() = default;
-    void addBlockId(block_id_t blockId) { address.push_back(blockId); }
-    block_id_t getBlockId(int index) const { return address[index]; }
-    int size() const { return address.size(); }
+    inline int size() const { return addresses.size(); }
+    inline block_id_t getBlockId(int index) const { return addresses[index]; }
+
+    inline void addBlockId(block_id_t blockId) { addresses.push_back(blockId); }
+
+private:
+    std::vector<block_id_t> addresses;
+
 };
 
 #endif /* address_h */
