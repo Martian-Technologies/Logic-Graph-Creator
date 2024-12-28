17217ce97e4b56f841058b66078438f2a8f08706
Ben Herman <benherman345@gmail.com>
2024-12-27 16:38:41 -0800

    added iter stuff

--- src/backend/container/blockContainer.h
+++ src/backend/container/blockContainer.h
@@ -60,6 +60,32 @@ public:
     // Trys to remove a connection. Returns if successful. Pass a Difference* to read the what changes were made.
     bool tryRemoveConnection(const Position& outputPosition, const Position& inputPosition, Difference* difference);
 
+    /* ----------- iterators ----------- */
+    // not safe if the container gets modifided (dont worry about it for now)
+    typedef std::unordered_map<block_id_t, Block>::iterator iterator;
+    typedef std::unordered_map<block_id_t, Block>::const_iterator const_iterator;
+    iterator begin() { return blocks.begin(); }
+    iterator end() { return blocks.end(); }
+    const_iterator begin() const { return blocks.begin(); }
+    const_iterator end() const { return blocks.end(); }
+
+    /* Difference Getter */
+    Difference getCreationDifference() const {
+        Difference difference;
+        for (auto iter : blocks) {
+            difference.addPlacedBlock(iter.second.getPosition(), iter.second.getRotation(), iter.second.type());
+        }
+        for (auto iter : blocks) {
+            for (connection_end_id_t id = 0; id <= iter.second.getConnectionContainer().getMaxConnectionId(); id++) {
+                if (iter.second.isConnectionInput(id)) continue;
+                for (auto connectionIter : iter.second.getConnectionContainer().getConnections(id)) {
+                    difference.addCreatedConnection(iter.second.getConnectionPosition(id).first, getBlock(connectionIter.getBlockId())->getConnectionPosition(connectionIter.getConnectionId()).first);
+                }
+            }
+        }
+        return difference;
+    }
+
 private:
     inline Block* getBlock(const Position& position);
     inline Block* getBlock(block_id_t blockId);
