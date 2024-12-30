fc3fcc20a4d6a7223c70febfa640567067f161a4
JackJ30 <jackqjamison@gmail.com>
2024-12-30 14:24:18 -0500

    initial PCH

---
+++ src/precompiled.h
@@ -0,0 +1,21 @@
+#ifndef LGC_PCH
+#define LGC_PCH
+
+#include <memory>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <optional>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+
+#include <sstream>
+#include <ostream>
+
+#endif
