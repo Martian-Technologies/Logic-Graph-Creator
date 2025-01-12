a57568b0bb53a2560737c0eca253f5910bac7404
niknal357 <46793548+niknal357@users.noreply.github.com>
2025-01-12 15:14:14 +0100

    working on eval

--- src/backend/evaluator/evaluatorDefs.h
+++ src/backend/evaluator/evaluatorDefs.h
@@ -3,19 +3,21 @@
 
 // this is so that we can have non-boolean logic states later because ben is actually insane (i think)
 typedef bool logic_state_t;
+typedef unsigned int input_socket_id_t;
+typedef unsigned int output_socket_id_t;
 typedef unsigned int eval_gate_id_t;
 
 enum class GateType {
 	NONE = 0,
-	AND = 8,
-	OR = 5,
-	XOR = 4,
-	NAND = 9,
-	NOR = 7,
+	AND = 1,
+	OR = 2,
+	XOR = 3,
+	NAND = 4,
+	NOR = 5,
 	XNOR = 6,
-	DEFAULT_RETURN_CURRENTSTATE = 1,
-	TICK_INPUT = 2,
-	CONSTANT_ON = 3,
+	DEFAULT_RETURN_CURRENTSTATE = 7,
+	TICK_INPUT = 8,
+	CONSTANT_ON = 9,
 };
 
 #endif // evaluatorDefs_h
--- src/backend/evaluator/logicSimulator.h
+++ src/backend/evaluator/logicSimulator.h
@@ -1,9 +1,75 @@
 #ifndef logicSimulator_h
 #define logicSimulator_h
 
+#include <barrier>
+#include <thread>
+#include <variant>
+
+#include "evaluatorDefs.h"
+
+struct Connection {
+    output_socket_id_t sourceSocket;
+    std::vector<input_socket_id_t> destinationSockets;
+};
+
+struct GateBothVector {
+    GateType type;
+    std::vector<input_socket_id_t> inputSockets;
+    std::vector<output_socket_id_t> outputSockets;
+};
+
+struct GateSingle {
+    GateType type;
+    input_socket_id_t inputSocket;
+    output_socket_id_t outputSocket;
+};
+
+struct GateInputVector {
+    GateType type;
+    std::vector<input_socket_id_t> inputSockets;
+    output_socket_id_t outputSocket;
+};
+
+struct GateOutputVector {
+    GateType type;
+    input_socket_id_t inputSocket;
+    std::vector<output_socket_id_t> outputSockets;
+};
+
+using Gate = std::variant<GateBothVector, GateSingle, GateInputVector, GateOutputVector>;
 
 class LogicSimulator {
-    
+public:
+    LogicSimulator()
+        : numThreads(4),
+        sync_after_state(numThreads),
+        sync_after_propagation(numThreads),
+        sync_after_swap(numThreads) {
+    }
+
+    output_socket_id_t registerOutputSocket();
+    std::vector<output_socket_id_t> registerOutputSockets(unsigned int count);
+    input_socket_id_t registerInputSocket(unsigned int count);
+    std::vector<input_socket_id_t> registerInputSockets(unsigned int count);
+    void registerGate(input_socket_id_t inputSocket, output_socket_id_t outputSocket, GateType type);
+    void registerGate(input_socket_id_t inputSocket, std::vector<output_socket_id_t> outputSockets, GateType type);
+    void registerGate(std::vector<input_socket_id_t> inputSockets, output_socket_id_t outputSocket, GateType type);
+    void registerGate(std::vector<input_socket_id_t> inputSockets, std::vector<output_socket_id_t> outputSockets, GateType type);
+    void connect(output_socket_id_t sourceSocket, input_socket_id_t destinationSocket);
+    void disconnect(output_socket_id_t sourceSocket, input_socket_id_t destinationSocket);
+
+private:
+    std::vector<logic_state_t> currentOutputSockets;
+    std::vector<logic_state_t> nextOutputSockets;
+    std::vector<unsigned int> inputCountSockets;
+    std::vector<Connection> connections;
+    std::vector<Gate> gates;
+
+    unsigned int numThreads;
+
+    std::barrier<> sync_after_state;
+    std::barrier<> sync_after_propagation;
+    std::barrier<> sync_after_swap;
 };
 
 #endif // logicSimulator_h
