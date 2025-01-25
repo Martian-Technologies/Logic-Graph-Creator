b7df5a0cc31eb0878afca8d24d39cf9dab808a41
JackJ30 <jackqjamison@gmail.com>
2025-01-24 22:44:43 -0500

    portability subset

--- src/gpu/vulkanPlatformBridge.cpp
+++ src/gpu/vulkanPlatformBridge.cpp
@@ -11,7 +11,8 @@
 std::vector<std::string> getRequiredInstanceExtensions() {
 #ifdef __APPLE__
 	// we don't have a library for this on apple, so this will have to work. May not age very well
-	std::vector<std::string> extensions = { "VK_KHR_surface", "VK_KHR_portability_enumeration", "VK_MVK_macos_surface"};
+	// we should expand the device picking code to set these more accurately
+	std::vector<std::string> extensions = { "VK_KHR_surface", "VK_KHR_portability_enumeration", "VK_KHR_portability_subset", "VK_MVK_macos_surface"};
 	return extensions;
 	
 #else
