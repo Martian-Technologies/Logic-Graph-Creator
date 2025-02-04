6e3e9887f9f6d6b66d49ec00f4a87e89bd4bae59
pchen2215 <pchen.2215@gmail.com>
2025-02-04 17:34:53 -0500

    cleaned up ansi

--- src/logging/logger.cpp
+++ src/logging/logger.cpp
@@ -1,5 +1,11 @@
 #include "logger.h"
 
+#define INFO_HEAD "[\033[1;37m"
+#define WARNING_HEAD "[\033[1;33m"
+#define ERROR_HEAD "[\033[1;31m"
+#define FATAL_HEAD "[\033[1;4;41;97m"
+#define ANSI_TAIL "\033[0m] "
+
 Logger::Logger(const std::filesystem::path& outputFile) : asyncStream(std::cout), outputFile(outputFile) {
 	outputFileStream.open(outputFile);
 	outputFileStream.close();
@@ -14,22 +20,22 @@ void Logger::log(LogType type, const std::string& message, const std::string& su
 	case LogType::Info:
 		thingThatGoesBetweenTheBrackets = "Info" + thingThatGoesBetweenTheBrackets;
 		fileBuffer << "[" + thingThatGoesBetweenTheBrackets + "] " + message + "\n";
-		asyncStream << "[\033[1;37m" + thingThatGoesBetweenTheBrackets + "\033[0m] " << message << "\n";
+		asyncStream << INFO_HEAD + thingThatGoesBetweenTheBrackets + ANSI_TAIL << message << "\n";
 		break;
 	case LogType::Warning:
 		thingThatGoesBetweenTheBrackets = "Warning" + thingThatGoesBetweenTheBrackets;
 		fileBuffer << "[" + thingThatGoesBetweenTheBrackets + "] " + message + "\n";
-		asyncStream << "[\033[1;33m" + thingThatGoesBetweenTheBrackets + "\033[0m] " << message << "\n";
+		asyncStream << WARNING_HEAD + thingThatGoesBetweenTheBrackets + ANSI_TAIL << message << "\n";
 		break;
 	case LogType::Error:
 		thingThatGoesBetweenTheBrackets = "ERROR" + thingThatGoesBetweenTheBrackets;
 		fileBuffer << "[" + thingThatGoesBetweenTheBrackets + "] " + message + "\n";
-		asyncStream << "[\033[1;31m" + thingThatGoesBetweenTheBrackets + "\033[0m] " << message << "\n";;
+		asyncStream << ERROR_HEAD + thingThatGoesBetweenTheBrackets + ANSI_TAIL << message << "\n";
 		break;
 	case LogType::Fatal:
 		thingThatGoesBetweenTheBrackets = "FATAL" + thingThatGoesBetweenTheBrackets;
 		fileBuffer << "[" + thingThatGoesBetweenTheBrackets + "] " + message + "\n";
-		asyncStream << "[\033[1;4;41;97m" + thingThatGoesBetweenTheBrackets + "\033[0m] " + message + "\n";
+		asyncStream << FATAL_HEAD + thingThatGoesBetweenTheBrackets + ANSI_TAIL + message + "\n";
 		break;
 	}
 	asyncStream.emit();
