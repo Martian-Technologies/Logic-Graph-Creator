3bf1377784ea15a996f9a17a0fea837ba661785a
pchen2215 <pchen.2215@gmail.com>
2025-02-04 17:04:07 -0500

    remove debug logging

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,6 @@ int main(int argc, char* argv[]) {
 	QApplication app(argc, argv);
 	app.setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
 
-	logInfo("wooho");
-	logInfo("woohotwo");
-	logError("oopsie");
-	logFatalError("big oopsie");
-	logWarning("no oopsie?");
-	logWarning("no oopsie?", "Vulkan");
-
 	MainWindow window;
 	window.show();
 
