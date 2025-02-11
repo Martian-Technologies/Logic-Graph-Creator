29bbceef5c005e8810644a14d7082cb7d294ef0a
NicholasCiuica <nick.ciuica@gmail.com>
2025-02-11 17:22:55 -0500

    More help dialog edits

--- src/gui/helpDialog.cpp
+++ src/gui/helpDialog.cpp
@@ -5,6 +5,7 @@ HelpDialog::HelpDialog(QWidget* parent) : QDialog(parent), ui(new Ui::HelpDialog
   ui->setupUi(this);
   ui->tableWidget->setColumnWidth(0, 300);
   ui->tableWidget->setColumnWidth(1, 500);
+  ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
   setWindowTitle("Help");
   resize(400, 200);
 }
