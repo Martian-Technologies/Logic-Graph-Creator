9654a33aff4680d75c1eff2f391722192f10eb0e
nathanc25 <nathan10776@gmail.com>
2025-03-21 17:43:20 -0400

    unit test - circuit placement

---
+++ tests/positionTests.h
@@ -0,0 +1,14 @@
+#ifndef circuitTests_h
+#define circuitTests_h
+
+#include <gtest/gtest.h>
+#include "backend/circuit/circuitManager.h"
+
+class CircuitTest: public ::testing::Test {
+protected:
+    void SetUp() override;
+    void TearDown() override;
+    int i;
+};
+
+#endif /* circuitTests_h */
