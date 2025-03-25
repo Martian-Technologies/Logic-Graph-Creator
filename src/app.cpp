c927aa62930de776d08d2f80d614ffb0b7a616f0
NicholasCiuica <nick.ciuica@gmail.com>
2025-03-25 17:28:43 -0400

    page structure prototype

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,7 @@
 #include "app.h"
 #include "gui/rml/RmlUi_Backend.h"
 #include "computerAPI/directoryManager.h"
-
+//
 App::App() {
 	Backend::Initialize("Gatality", 800, 600,  true);
 	
@@ -12,9 +12,9 @@ App::App() {
 	
 	rmlContext = Rml::CreateContext("main", Rml::Vector2i(800, 600));
 
-	Rml::LoadFontFace(DirectoryManager::getResourceDirectory() / "gui/fonts/monaspace.otf");
+	Rml::LoadFontFace((DirectoryManager::getResourceDirectory() / "gui/fonts/monaspace.otf").string());
 	
-	Rml::ElementDocument* document = rmlContext->LoadDocument(DirectoryManager::getResourceDirectory() / "gui/mainwindow.rml");
+	Rml::ElementDocument* document = rmlContext->LoadDocument((DirectoryManager::getResourceDirectory() / "gui/mainwindow.rml").string());
 
 	document->Show();
 }
