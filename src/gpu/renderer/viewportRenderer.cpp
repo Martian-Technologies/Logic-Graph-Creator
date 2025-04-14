56990f345d1309b3132b5a2953f7ffeba2f7d729
JackJ30 <jackqjamison@gmail.com>
2025-04-13 22:00:07 -0400

    rename v2

---
+++ src/gpu/renderer/viewportRenderer.cpp
@@ -0,0 +1,87 @@
+#include "viewportRenderer.h"
+
+#include <glm/ext/matrix_clip_space.hpp>
+
+#include "gpu/renderer/windowRenderer.h"
+
+ViewportRenderer::ViewportRenderer(WindowRenderer* windowRenderer, Rml::Element* element)
+	: windowRenderer(windowRenderer), element(element) {
+
+	windowRenderer->registerViewportRenderInterface(this);
+}
+
+ViewportRenderer::~ViewportRenderer() {
+	windowRenderer->deregisterViewportRenderInterface(this);
+}
+
+
+void ViewportRenderer::initializeVulkan(VkRenderPass renderPass) {
+	chunkRenderer = std::make_unique<VulkanChunkRenderer>(renderPass);
+}
+
+void ViewportRenderer::render(VulkanFrameData& frame) {
+
+	// logInfo("({}, {})", "Vulkan", element->GetBox().GetSize().x, element->GetBox().GetSize().y);
+}
+
+
+void ViewportRenderer::setCircuit(Circuit* circuit) {
+	
+}
+void ViewportRenderer::setEvaluator(Evaluator* evaluator) {
+	
+}
+void ViewportRenderer::updateView(ViewManager* viewManager) {
+	std::lock_guard<std::mutex> lock(viewMux);
+	
+	// Update vulkan viewport
+	viewData.viewport.x = element->GetAbsoluteOffset().x;
+	viewData.viewport.y = element->GetAbsoluteOffset().y;
+	viewData.viewport.width = element->GetBox().GetSize().x;
+	viewData.viewport.height = element->GetBox().GetSize().y;
+	viewData.viewport.minDepth = 0.0f;
+	viewData.viewport.maxDepth = 1.0f;
+
+	// Create view mat
+	FPosition topLeft = viewManager->getTopLeft();
+	FPosition bottomRight = viewManager->getBottomRight();
+	viewData.viewportViewMat = glm::ortho(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y);
+	viewData.viewBounds = { topLeft, bottomRight };
+}
+void ViewportRenderer::updateCircuit(DifferenceSharedPtr diff) {
+	
+}
+float ViewportRenderer::getLastFrameTimeMs() const {
+	return 0.0f;
+}
+ElementID ViewportRenderer::addSelectionElement(const SelectionObjectElement& selection) {
+	return 0;
+}
+ElementID ViewportRenderer::addSelectionElement(const SelectionElement& selection) {
+	return 0;
+}
+void ViewportRenderer::removeSelectionElement(ElementID selection) {
+	
+}
+ElementID ViewportRenderer::addBlockPreview(const BlockPreview& blockPreview) {
+	return 0;
+}
+void ViewportRenderer::removeBlockPreview(ElementID blockPreview) {
+	
+}
+ElementID ViewportRenderer::addConnectionPreview(const ConnectionPreview& connectionPreview) {
+	return 0;
+}
+void ViewportRenderer::removeConnectionPreview(ElementID connectionPreview) {
+	
+}
+ElementID ViewportRenderer::addHalfConnectionPreview(const HalfConnectionPreview& halfConnectionPreview) {
+	return 0;
+}
+void ViewportRenderer::removeHalfConnectionPreview(ElementID halfConnectionPreview) {
+	
+}
+void ViewportRenderer::spawnConfetti(FPosition start) {
+
+}
+
---
+++ src/gpu/renderer/viewportRenderer.h
@@ -0,0 +1,66 @@
+#ifndef viewportRenderer_h
+#define viewportRenderer_h
+
+#include <RmlUi/Core/Element.h>
+#include <vulkan/vulkan.h>
+
+#include "backend/circuitView/renderer/renderer.h"
+#include "gpu/renderer/vulkanChunkRenderer.h"
+#include "gpu/renderer/vulkanFrame.h"
+
+struct WindowRenderer;
+
+struct ViewportViewData {
+	glm::mat4 viewportViewMat;
+	std::pair<FPosition, FPosition> viewBounds;
+	VkViewport viewport;
+};
+
+class ViewportRenderer : public Renderer {
+public:
+	ViewportRenderer(WindowRenderer* windowRenderer, Rml::Element* element);
+	~ViewportRenderer();
+
+	void initializeVulkan(VkRenderPass renderPass);
+	void render(VulkanFrameData& frame);
+	
+public:
+	// main flow
+	void setCircuit(Circuit* circuit) override;
+	void setEvaluator(Evaluator* evaluator) override;
+
+	void updateView(ViewManager* viewManager) override;
+	void updateCircuit(DifferenceSharedPtr diff) override;
+
+	float getLastFrameTimeMs() const override;
+
+private:
+	// elements
+	ElementID addSelectionElement(const SelectionObjectElement& selection) override;
+	ElementID addSelectionElement(const SelectionElement& selection) override;
+	void removeSelectionElement(ElementID selection) override;
+
+	ElementID addBlockPreview(const BlockPreview& blockPreview) override;
+	void removeBlockPreview(ElementID blockPreview) override;
+
+	ElementID addConnectionPreview(const ConnectionPreview& connectionPreview) override;
+	void removeConnectionPreview(ElementID connectionPreview) override;
+
+	ElementID addHalfConnectionPreview(const HalfConnectionPreview& halfConnectionPreview) override;
+	void removeHalfConnectionPreview(ElementID halfConnectionPreview) override;
+
+	void spawnConfetti(FPosition start) override;
+
+private:
+	// From the UI Side
+	WindowRenderer* windowRenderer;
+	Rml::Element* element;
+
+	// Vulkan
+	std::unique_ptr<VulkanChunkRenderer> chunkRenderer = nullptr;
+
+	ViewportViewData viewData;
+	std::mutex viewMux;
+};
+
+#endif
