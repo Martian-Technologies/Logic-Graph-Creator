734a84d27d214f179f8d18ebb8ea12dbc5a02db8
JackJ30 <jackqjamison@gmail.com>
2025-04-14 22:16:35 -0400

    improve blending for text rendering

--- src/gpu/renderer/vulkanPipeline.cpp
+++ src/gpu/renderer/vulkanPipeline.cpp
@@ -76,7 +76,7 @@ Pipeline::Pipeline(const PipelineInformation& info) {
 	VkPipelineColorBlendAttachmentState colorBlendAttachment{};
 	colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 	colorBlendAttachment.blendEnable = VK_TRUE;
-	colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+	colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
 	colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
 	colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
 	colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
