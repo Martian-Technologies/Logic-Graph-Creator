ee0494ed85917b09bc096f8ad7fe79226232bfe5
Ben Herman <benherman345@gmail.com>
2025-04-17 00:04:35 -0400

    start to IC creation

--- src/gui/blockCreationWindow.cpp
+++ src/gui/blockCreationWindow.cpp
@@ -64,12 +64,14 @@ void BlockCreationWindow::resetMenu() {
 	Circuit* circuit = circuitViewWidget->getCircuitView()->getCircuit();
 	if (!circuit) return;
 	circuit_id_t id = circuit->getCircuitId();
-	const CircuitBlockData* circuitBlockData = circuitManager->getCircuitBlockDataManager()->getCircuitBlockData(id);
 	Rml::Element* ele = menu->GetElementById("name-input");
 	Rml::ElementFormControlInput* nameElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(ele);
 	nameElement->SetValue(circuit->getCircuitName());
-
+	
+	const CircuitBlockData* circuitBlockData = circuitManager->getCircuitBlockDataManager()->getCircuitBlockData(id);
+	if (!circuitBlockData) return;
 	const BlockData* blockData = circuitManager->getBlockDataManager()->getBlockData(circuitBlockData->getBlockType());
+	if (!blockData) return;
 
 	ele = menu->GetElementById("width-input");
 	Rml::ElementFormControlInput* widthElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(ele);
@@ -77,6 +79,86 @@ void BlockCreationWindow::resetMenu() {
 	ele = menu->GetElementById("height-input");
 	Rml::ElementFormControlInput* heightElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(ele);
 	heightElement->SetValue(std::to_string(blockData->getSize().dy));
+
+	const std::unordered_map<connection_end_id_t, std::pair<Vector, bool>>& conncections = blockData->getConnections();
+	for (auto& iter : conncections) {
+		connection_end_id_t endId = iter.first;
+		bool isInputBool = iter.second.second;
+		const Vector& positionOnBlock = iter.second.first;
+		const std::string* connectionNamePtr = circuitBlockData->getConnectionIdToName(endId);
+		std::string connectionName;
+		if (connectionNamePtr) {
+			connectionName = *connectionNamePtr;
+		} else {
+			connectionName = "None";
+		}
+		const Position* positionPtr = circuitBlockData->getConnectionIdToPosition(endId);
+		Rml::ElementPtr row = document->CreateElement("div");
+		// name
+		Rml::XMLAttributes nameAttributes;
+		nameAttributes["type"] = "text";
+		nameAttributes["maxlength"] = "10";
+		nameAttributes["size"] = "10";
+		// nameAttributes["class"] = "name";
+		Rml::ElementPtr name = Rml::Factory::InstanceElement(document, "input", "input", nameAttributes);
+		Rml::ElementFormControlInput* nameElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(name.get());
+		nameElement->SetValue(connectionName);
+		// isInput
+		Rml::XMLAttributes isInputAttributes;
+		isInputAttributes["type"] = "checkbox";
+		if (isInputBool) isInputAttributes["checked"] = "";
+		Rml::ElementPtr isInput = Rml::Factory::InstanceElement(document, "input", "input", isInputAttributes);
+		// positionOnBlock
+		Rml::XMLAttributes positionOnBlockAttributes;
+		positionOnBlockAttributes["type"] = "text";
+		positionOnBlockAttributes["maxlength"] = "3";
+		positionOnBlockAttributes["size"] = "2";
+		// positionOnBlockAttributes["class"] = "number";
+		Rml::ElementPtr positionOnBlockX = Rml::Factory::InstanceElement(document, "input", "input", positionOnBlockAttributes);
+		Rml::ElementPtr positionOnBlockY = Rml::Factory::InstanceElement(document, "input", "input", positionOnBlockAttributes);
+		Rml::ElementFormControlInput* positionOnBlockXElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(positionOnBlockX.get());
+		Rml::ElementFormControlInput* positionOnBlockYElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(positionOnBlockY.get());
+		positionOnBlockXElement->SetValue(std::to_string(positionOnBlock.dx));
+		positionOnBlockYElement->SetValue(std::to_string(positionOnBlock.dy));
+		// position
+		Rml::XMLAttributes positionAttributes;
+		positionAttributes["type"] = "text";
+		positionAttributes["maxlength"] = "6";
+		positionAttributes["size"] = "5";
+		// positionAttributes["class"] = "number-long";
+		Rml::ElementPtr positionX = Rml::Factory::InstanceElement(document, "input", "input", positionAttributes);
+		Rml::ElementPtr positionY = Rml::Factory::InstanceElement(document, "input", "input", positionAttributes);
+		Rml::ElementFormControlInput* positionXElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(positionX.get());
+		Rml::ElementFormControlInput* positionYElement = rmlui_dynamic_cast<Rml::ElementFormControlInput*>(positionY.get());
+		if (positionPtr) {
+			positionXElement->SetValue(std::to_string(positionPtr->x));
+			positionYElement->SetValue(std::to_string(positionPtr->y));
+		} else {
+			positionXElement->SetValue("N/A");
+			positionYElement->SetValue("N/A");
+		}
+		// Rml::ElementPtr ele = document->CreateElement("div");
+		row->AppendChild(std::move(name));
+		// row->AppendChild(std::move(ele));
+		// ele = document->CreateElement("div");
+		row->AppendChild(std::move(isInput));
+		// row->AppendChild(std::move(ele));
+		// ele = document->CreateElement("div");
+		row->AppendChild(std::move(positionOnBlockX));
+		// row->AppendChild(std::move(ele));
+		// ele = document->CreateElement("div");
+		row->AppendChild(std::move(positionOnBlockY));
+		// row->AppendChild(std::move(ele));
+		// ele = document->CreateElement("div");
+		row->AppendChild(std::move(positionX));
+		// row->AppendChild(std::move(ele));
+		// ele = document->CreateElement("div");
+		row->AppendChild(std::move(positionY));
+		// row->AppendChild(std::move(ele));
+		row->SetClass("connection-list-item", true);
+		row->SetId("ConnectionListItem Id: " + std::to_string(endId));
+		list->AppendChild(std::move(row));
+	}
 }
 
 void BlockCreationWindow::makePaths(std::vector<std::vector<std::string>>& paths, std::vector<std::string>& path, const AddressTreeNode<Evaluator::EvaluatorGate>& addressTree) {
