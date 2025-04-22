450b4614cbf235f0fab900de21c274fcd5119bfc
Ben Herman <benherman345@gmail.com>
2025-04-22 15:45:27 -0400

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 #include "computerAPI/directoryManager.h"
 #include "app.h"
 
+#if defined(_WIN32) 
+int SDL_main(int argc, char* argv[]) {
+#else
 int main(int argc, char* argv[]) {
+#endif
 	try {
 		// Set up directory manager
 		DirectoryManager::findDirectories();
