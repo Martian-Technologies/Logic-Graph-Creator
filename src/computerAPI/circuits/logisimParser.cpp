b6cf37382cff357c017f4050a2f79a2fc2a10ac3
August Bernberg <abearnmountain@gmail.com>
2025-05-09 11:33:42 -0700

    Logisim Parser Interface

---
+++ src/computerAPI/circuits/logisimParser.cpp
@@ -0,0 +1,96 @@
+#include "logisimParser.h"
+#include "backend/circuit/circuit.h"
+#include "backend/circuit/parsedCircuit.h"
+#include <fstream>
+#include <string>
+
+
+std::vector<circuit_id_t> LogisimParser::load(const std::string& path) {
+	// Check for cyclic import
+	if (importedFiles.find(path) != importedFiles.end()) {
+		logError("Cyclic import detected: " + path, "GatalityParser");
+		return {};
+	}
+	importedFiles.insert(path);
+	logInfo("Parsing Logisim Circuit File (.circ)", "LogisimParser");
+
+	std::ifstream inputFile(path, std::ios::in);
+	if (!inputFile.is_open()) {
+		logError("Couldn't open file at path: " + path, "LogisimParser");
+		return {};
+	}
+
+	logInfo("Inserted current file as dependency: " + path, "LogisimParser");
+
+	// utility
+	auto trim = [](std::string& s) { // remoes excessive spaces and bloat from a line
+		s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+			return !std::isspace(ch);
+		}));
+		s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+			return !std::isspace(ch);
+		}).base(), s.end());
+	};
+
+	std::string token;
+	char cToken;
+	int debugLine = 1;
+
+	std::string circuitName;
+	bool parsingCircuit;
+	std::vector<std::string> circuitChunk;
+	
+	std::vector<circuit_id_t> circuitIds;
+	std::vector<SharedParsedCircuit> circuitList;
+
+	while (std::getline(inputFile, token)) {
+		trim(token);
+
+		// finds circuit, else hops over trash
+		if (!parsingCircuit && token.substr(1, 7) == "circuit") {
+			size_t namePos = token.find("name=\"") + 6;
+			circuitName = token.substr(namePos, token.find("\"", namePos) - namePos);
+
+			parsingCircuit = true;
+				
+			logInfo("Reading Circuit: " + circuitName + " at line " + std::to_string(debugLine), "LogisimParser");
+		} 
+
+		if (parsingCircuit) {
+			// finds start and end of xml <circuit>, throws that to loadCircuit func
+			if (token.substr(1, 7) == "circuit") {
+				parsingCircuit = false;
+				loadCircuit(circuitChunk);
+			} else {
+				circuitChunk.push_back(token);
+			}
+		}
+
+		debugLine++;
+	}
+
+	if (circuitList[0]) {
+		circuit_id_t circuitId = loadParsedCircuit(circuitList[0]);
+		if (circuitId != 0) circuitIds.push_back(circuitId);
+	}
+
+	inputFile.close();
+	importedFiles.erase(path);
+	return circuitIds;
+}
+
+void loadCircuit();
+
+void loadWire() {
+	
+}
+
+void loadComp() {
+	
+}
+
+
+bool save(const CircuitFileManager::FileData& fileData, bool compress) {
+	logWarning("not supported yet");
+	return false;
+}
---
+++ src/computerAPI/circuits/logisimParser.h
@@ -0,0 +1,92 @@
+#ifndef LOGISIM_PARSER_H
+#define LOGISIM_PARSER_H
+
+/* parses LOGISIM's .circ files */
+/* ONLY SUPPORTS IMPORT RIGHT NOW, NO EXPORTING AVAILABLE */
+
+#include "backend/circuit/circuit.h"
+#include "backend/circuit/parsedCircuit.h"
+#include "backend/container/block/block.h"
+#include "backend/container/block/blockDefs.h"
+#include "backend/position/position.h"
+#include "computerAPI/circuits/parsedCircuitLoader.h"
+#include <unordered_map>
+
+// this is for the visual appearance of the circuit
+struct XMLAppearance {
+
+};
+
+// each gate
+struct XMLComponent {
+/*  '*' represents implemented, others have to get implemented at some point
+Format: "<comp lib="0" loc="(60,300)" name="Pin">"
+	* lib="0": Wiring Library
+		- non-logic components
+			- pins, wires, power and ground, clocks, tunnels, pull resistors
+	* lib="1": Gate Library
+		- basic logic gates
+			-
+			-
+			- 
+	lib="2": Plexer Library
+		- selecitng and routing signals
+			- multiplexers, demultiplexers, decoders, encoders, bit extenders
+	lib="3": Arithmetic Library
+		- components for performing arithmetic operations
+			- adders, subtractors, multipliers, dividers, comparators, shifters
+
+	lib="4": Memory Library
+		- memory elements
+			- RAM, ROM, Registers, Counters
+
+	lib="5": Input/Output Library
+		- graphical outputs for users
+			- buttons, switches, leds, hex digit displays, tty(not going to suppor this most likely)
+
+	lib="6": Base Library
+		- Fundamental and Structural Elements
+			- text labels, closed boxes, splitters
+
+	lib="7": User Custom Libraries 
+		- defined by user, incrementing form 7+i for each additional library added
+*/
+
+	uint8_t lib; // not required but could be helpful 
+	// all values where | <a name="*" | appears
+	std::string label; 
+	
+	
+};
+
+// wires a - b, should cut otu intermediates
+// TODO: fully reduce excess wires
+struct XMLWire {
+	int x1, y1;
+	int x2, y2;
+};
+
+class LogisimParser : public ParsedCircuitLoader {
+public:
+	
+    LogisimParser(CircuitFileManager* circuitFileManager, CircuitManager* circuitManager) : ParsedCircuitLoader(circuitFileManager, circuitManager) {}
+
+	std::vector<circuit_id_t> load(const std::string& path) override;
+    bool save(const CircuitFileManager::FileData& fileData, bool compress);
+
+private:
+
+	void loadCircuit(const std::vector<std::string>& circuitChunk); // loads xml "<circuit name=...>"
+	void loadAppearance(); // loads xml <appear> block
+	void loadWire(); // loads xml "<wire from=... to=... />"
+	void loadComp(); // loads xml "<comp from=... to=...> ... </comp>"
+
+	std::unordered_set<std::string> importedFiles;	
+	std::unordered_map<std::string, BlockType> blockReference = {
+		{ "AND Gate", BlockType::AND },
+		{ "OR Gate", BlockType::OR },
+		{ "NOT Gate", BlockType::NAND },
+		{ "NAND Gate", BlockType::NAND }
+	};
+};
+#endif
