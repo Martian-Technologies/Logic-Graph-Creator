668b6eb835e54b6ae19f1ce7a72e76906e7430c1
ItchyTrack <benherman345@gmail.com>
2025-05-14 13:24:01 -0700

    mac build working

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -75,153 +75,153 @@ const std::vector<std::string>& Settings::getKeybindGraphicsData() {
 }
 
 void Settings::serializeData() {
-	std::ifstream file(USER_CONFIG);
-	if (!file) {
-		logWarning("user config not found");
-	}
+	// std::ifstream file(USER_CONFIG);
+	// if (!file) {
+	// 	logWarning("user config not found");
+	// }
 
-	// TODO: read arrays and fix file errors
+	// // TODO: read arrays and fix file errors
 
-	// removes white spaces from word
+	// // removes white spaces from word
 
-	auto trim = [](std::string& word) -> std::string& {
-		auto start = std::find_if(word.begin(), word.end(), [](unsigned char c) {
-			return !std::isspace(c);
-		});
+	// auto trim = [](std::string& word) -> std::string& {
+	// 	auto start = std::find_if(word.begin(), word.end(), [](unsigned char c) {
+	// 		return !std::isspace(c);
+	// 	});
 
-		auto end = std::find_if(word.rbegin(), word.rend(), [](unsigned char c) {
-			return !std::isspace(c);
-		}).base(); 
+	// 	auto end = std::find_if(word.rbegin(), word.rend(), [](unsigned char c) {
+	// 		return !std::isspace(c);
+	// 	}).base(); 
 
-		if (start >= end) {
-			word.clear();
-		} else {
-			word = word.substr(std::distance(word.begin(), start), std::distance(start, end));
-		}
+	// 	if (start >= end) {
+	// 		word.clear();
+	// 	} else {
+	// 		word = word.substr(std::distance(word.begin(), start), std::distance(start, end));
+	// 	}
 
-		return word;
-	};
+	// 	return word;
+	// };
 
 
 
-	int lineNumber = 0;
-	std::string prefix;
-	std::string line;
+	// int lineNumber = 0;
+	// std::string prefix;
+	// std::string line;
 
-	while (std::getline(file, line)) {
-		trim(line);
-		if (line.empty() || line[0] == '#') continue; 
-		if (line[0] == '[') {
-			prefix = "";
-			for (int i = 1; i < line.size(); i++) {
-				if (line[i] == ']') break;
-				prefix += line[i];
-			}
-			continue;
-		}
+	// while (std::getline(file, line)) {
+	// 	trim(line);
+	// 	if (line.empty() || line[0] == '#') continue; 
+	// 	if (line[0] == '[') {
+	// 		prefix = "";
+	// 		for (int i = 1; i < line.size(); i++) {
+	// 			if (line[i] == ']') break;
+	// 			prefix += line[i];
+	// 		}
+	// 		continue;
+	// 	}
 
-		size_t equality = line.find('=');
+	// 	size_t equality = line.find('=');
 
-		// ----- Gets Key -----
-		std::string key = line.substr(0, equality);
-		trim(key);
+	// 	// ----- Gets Key -----
+	// 	std::string key = line.substr(0, equality);
+	// 	trim(key);
 
-		// ----- Gets Value -----
-		int end = line.size();
-		for (int i = 0; i < end - 1; i++) {
-			if (line[i] == ' ' && line[i + 1] == '#') end = i;
-		}
-		std::string value = line.substr(equality + 1, end);
-		trim(value);
+	// 	// ----- Gets Value -----
+	// 	int end = line.size();
+	// 	for (int i = 0; i < end - 1; i++) {
+	// 		if (line[i] == ' ' && line[i + 1] == '#') end = i;
+	// 	}
+	// 	std::string value = line.substr(equality + 1, end);
+	// 	trim(value);
 
-		if (value.substr(1,2) == "0x") 				   CONFIG_SETTINGS.set<Color>(prefix + '.' + key, Color(std::stoi(value.substr(3,2), nullptr, 16)/255.0f, std::stoi(value.substr(5,2), nullptr, 16)/255.0f, std::stoi(value.substr(7,2), nullptr, 16)/255.0f));
-		else if (value == "true"  || value == "True")  CONFIG_SETTINGS.set<bool>(prefix + '.' + key, 1);
-		else if (value == "false" || value == "False") CONFIG_SETTINGS.set<bool>(prefix + '.' + key, 0);
-		else										   CONFIG_SETTINGS.set<std::string>(prefix + '.' + key, value.substr(1, value.size() - 2)); 
+	// 	if (value.substr(1,2) == "0x") 				   CONFIG_SETTINGS.set<Color>(prefix + '.' + key, Color(std::stoi(value.substr(3,2), nullptr, 16)/255.0f, std::stoi(value.substr(5,2), nullptr, 16)/255.0f, std::stoi(value.substr(7,2), nullptr, 16)/255.0f));
+	// 	else if (value == "true"  || value == "True")  CONFIG_SETTINGS.set<bool>(prefix + '.' + key, 1);
+	// 	else if (value == "false" || value == "False") CONFIG_SETTINGS.set<bool>(prefix + '.' + key, 0);
+	// 	else										   CONFIG_SETTINGS.set<std::string>(prefix + '.' + key, value.substr(1, value.size() - 2)); 
 
-		lineNumber++;
-	}
+	// 	lineNumber++;
+	// }
 
-	file.close();
+	// file.close();
 }
 
 
 void Settings::deserializeDate() {
-	std::ofstream file("resources/config.toml", std::ios::trunc);
-	std::string value;
+	// std::ofstream file("resources/config.toml", std::ios::trunc);
+	// std::string value;
 
 
-	auto toHex = [](float component) -> std::string {
-        int intValue = static_cast<int>(std::round(component * 255));
-        std::ostringstream hexStream;
-        hexStream << std::hex << std::setw(2) << std::setfill('0') << intValue;
-        return hexStream.str();
-    };
+	// auto toHex = [](float component) -> std::string {
+    //     int intValue = static_cast<int>(std::round(component * 255));
+    //     std::ostringstream hexStream;
+    //     hexStream << std::hex << std::setw(2) << std::setfill('0') << intValue;
+    //     return hexStream.str();
+    // };
 
-	auto getKey = [](std::string component) -> std::string {
-		return component.substr(component.rfind('.')+1);
-	};
+	// auto getKey = [](std::string component) -> std::string {
+	// 	return component.substr(component.rfind('.')+1);
+	// };
 
 
-	for (uint8_t i = 0; i < 3; i++) {
-		switch(i) {
-			case 0:
-				file << "# -------------------- General --------------------";
-				break;
-			case 1:
-				file << "# -------------------- Appearance --------------------";
-				break;
-			case 2:
-				file << "# -------------------- Keybinds --------------------";
-				break;
-			default:
-				break;
-		}
+	// for (uint8_t i = 0; i < 3; i++) {
+	// 	switch(i) {
+	// 		case 0:
+	// 			file << "# -------------------- General --------------------";
+	// 			break;
+	// 		case 1:
+	// 			file << "# -------------------- Appearance --------------------";
+	// 			break;
+	// 		case 2:
+	// 			file << "# -------------------- Keybinds --------------------";
+	// 			break;
+	// 		default:
+	// 			break;
+	// 	}
 
-		for (uint8_t j = 0; j < 32; j++) {
-			value.clear();
+	// 	for (uint8_t j = 0; j < 32; j++) {
+	// 		value.clear();
 
-			if (i == 0) {
-				if (general[j].empty()) break;
+	// 		if (i == 0) {
+	// 			if (general[j].empty()) break;
 
-				if (general[j][1] == "HEADER") {
-					file << "\n[general." << std::string(1, std::tolower(static_cast<unsigned char>(general[j][0][0]))) << general[j][0].substr(1) << "]\n";
-					continue;
-				}
+	// 			if (general[j][1] == "HEADER") {
+	// 				file << "\n[general." << std::string(1, std::tolower(static_cast<unsigned char>(general[j][0][0]))) << general[j][0].substr(1) << "]\n";
+	// 				continue;
+	// 			}
 
-				value = getKey(general[j][0]) + " = \"" + Settings::get<std::string>(general[j][0]).value() + "\"";
-			} else if (i == 1) {
-				if (appearance[j].empty()) break;
+	// 			value = getKey(general[j][0]) + " = \"" + Settings::get<std::string>(general[j][0]).value() + "\"";
+	// 		} else if (i == 1) {
+	// 			if (appearance[j].empty()) break;
 
-				if (appearance[j][1] == "HEADER") {
-					file << "\n[appearance." << std::string(1, std::tolower(static_cast<unsigned char>(appearance[j][0][0]))) << appearance[j][0].substr(1) << "]\n";
-					continue;
-				}
+	// 			if (appearance[j][1] == "HEADER") {
+	// 				file << "\n[appearance." << std::string(1, std::tolower(static_cast<unsigned char>(appearance[j][0][0]))) << appearance[j][0].substr(1) << "]\n";
+	// 				continue;
+	// 			}
 
-				value += getKey(appearance[j][0]) + " = \"";
-				if (appearance[j][1] == "COLOR") {
-					Color clr = Settings::get<Color>(appearance[j][0]).value();
-					value += "0x" + toHex(clr.r) + toHex(clr.g) + toHex(clr.b);
-				} else if (appearance[j][1] == "USERINPUT") {
-					value += Settings::get<std::string>(appearance[j][0]).value();
-				}	
-				value += "\"";
-			} else if (i == 2) {
-				if (keybind[j].empty()) break;
+	// 			value += getKey(appearance[j][0]) + " = \"";
+	// 			if (appearance[j][1] == "COLOR") {
+	// 				Color clr = Settings::get<Color>(appearance[j][0]).value();
+	// 				value += "0x" + toHex(clr.r) + toHex(clr.g) + toHex(clr.b);
+	// 			} else if (appearance[j][1] == "USERINPUT") {
+	// 				value += Settings::get<std::string>(appearance[j][0]).value();
+	// 			}	
+	// 			value += "\"";
+	// 		} else if (i == 2) {
+	// 			if (keybind[j].empty()) break;
 
-				if (keybind[j].substr(0,2) == "H_") {
-					file << "\n[keybind." << std::string(1, std::tolower(static_cast<unsigned char>(keybind[j][2]))) << keybind[j].substr(3) << "]\n";
-					continue;
-				}
+	// 			if (keybind[j].substr(0,2) == "H_") {
+	// 				file << "\n[keybind." << std::string(1, std::tolower(static_cast<unsigned char>(keybind[j][2]))) << keybind[j].substr(3) << "]\n";
+	// 				continue;
+	// 			}
 
-				value = getKey(keybind[j]) + " = \"" + Settings::get<std::string>(keybind[j]).value() + "\"";
-			}
-			file << value << '\n';
-		}
-		file << '\n';
-	}
+	// 			value = getKey(keybind[j]) + " = \"" + Settings::get<std::string>(keybind[j]).value() + "\"";
+	// 		}
+	// 		file << value << '\n';
+	// 	}
+	// 	file << '\n';
+	// }
 	
-	file.close();
+	// file.close();
 }
 
 MultiTypeMap& Settings::getConfig() { return CONFIG_SETTINGS; }
