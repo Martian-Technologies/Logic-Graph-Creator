e5eae448c742106e0e1a2d6478a3b4e73b1ac2ce
ItchyTrack <benherman345@gmail.com>
2025-05-28 17:58:58 -0700

    fixed setting template

--- src/backend/settings/settings.cpp
+++ src/backend/settings/settings.cpp
@@ -2,30 +2,4 @@
 
 SettingsMap settingsMap;
 
-template<SettingType settingType>
-void Settings::registerName(std::string name) {
-	settingsMap.registerName<settingType>(name);
-}
-
-template<SettingType settingType>
-void Settings::registerName(std::string name, const typename SettingTypeToType<settingType>::type& value) {
-	settingsMap.registerName(name, value);
-}
-
-template<SettingType settingType>
-const SettingTypeToType<settingType>::type* get(const std::string& key) {
-	return settingsMap.get<settingType>(key);
-}
-
-template<SettingType settingType>
-bool Settings::set(const std::string& key, const typename SettingTypeToType<settingType>::type& value) {
-	return settingsMap.set(key, value);
-}
-
-const SettingType Settings::getType(const std::string& key) {
-	return settingsMap.getType(key);
-}
-
-bool Settings::hasKey(const std::string& key) {
-	return settingsMap.hasKey(key);
-}
+SettingsMap& Settings::getSettingsMap() { return settingsMap; }
--- src/backend/settings/settings.h
+++ src/backend/settings/settings.h
@@ -4,16 +4,29 @@
 #include "settingsMap.h"
 
 namespace Settings {
+	SettingsMap& getSettingsMap();
 	template<SettingType settingType>
-	void registerName(std::string name);
+	inline void registerName(std::string name) {
+		getSettingsMap().registerName<settingType>(name);
+	}
 	template<SettingType settingType>
-	void registerName(std::string name, const typename SettingTypeToType<settingType>::type& value);
+	inline void registerName(std::string name, const typename SettingTypeToType<settingType>::type& value) {
+		getSettingsMap().registerName(name, value);
+	}
 	template<SettingType settingType>
-	const typename SettingTypeToType<settingType>::type* get(const std::string& key);
+	inline const SettingTypeToType<settingType>::type* get(const std::string& key) {
+		return getSettingsMap().get<settingType>(key);
+	}
 	template<SettingType settingType>
-	bool set(const std::string& key, const typename SettingTypeToType<settingType>::type& value);
-	const SettingType getType(const std::string& key);
-	bool hasKey(const std::string& key);
+	inline bool set(const std::string& key, const typename SettingTypeToType<settingType>::type& value) {
+		return getSettingsMap().set(key, value);
+	}
+	inline SettingType getType(const std::string& key) {
+		return getSettingsMap().getType(key);
+	}
+	inline bool hasKey(const std::string& key) {
+		return getSettingsMap().hasKey(key);
+	}
 };
 
 #endif /* settings_h */
