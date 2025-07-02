bcbae6adecb83e2001a9f2f790ad1b62669fad13
niknal357 <46793548+niknal357@users.noreply.github.com>
2025-07-02 18:26:30 +0200

    fix

--- src/backend/evaluator/simulatorGate.h
+++ src/backend/evaluator/simulatorGate.h
@@ -32,26 +32,32 @@ struct GateTypeConfig {
 
 inline const GateTypeConfig& getGateTypeConfig(GateType type) {
 	switch (type) {
-		case GateType::NONE:
-			return GateTypeConfig(0, 0);
-		case GateType::TICK_INPUT:
-			return GateTypeConfig(0, 1);
-		case GateType::CONSTANT_ON:
-			return GateTypeConfig(0, 1);
-		case GateType::TRISTATE_BUFFER:
-			return GateTypeConfig(2, 1);
-		case GateType::TRISTATE_BUFFER_INVERTED:
-			return GateTypeConfig(2, 1);
-		default:
-			return GateTypeConfig(1, 1);
+	case GateType::NONE:
+		return GateTypeConfig(0, 0);
+	case GateType::TICK_INPUT:
+		return GateTypeConfig(0, 1);
+	case GateType::CONSTANT_ON:
+		return GateTypeConfig(0, 1);
+	case GateType::TRISTATE_BUFFER:
+		return GateTypeConfig(2, 1);
+	case GateType::TRISTATE_BUFFER_INVERTED:
+		return GateTypeConfig(2, 1);
+	default:
+		return GateTypeConfig(1, 1);
 	}
-}
+};
 
-struct BGate { // includes AND / OR / NAND / NOR
-
+class BasicGate { // includes AND / OR / NAND / NOR
+public:
+	BasicGate(GateType type) {
+		inputsInverted = (type == GateType::OR || type == GateType::NOR);
+		outputsInverted = (type == GateType::NAND || type == GateType::OR);
+	};
 
 private:
 	std::vector<GateConnection> inputs;
-}
+	bool inputsInverted;
+	bool outputsInverted;
+};
 
 #endif // simulatorGate_h
