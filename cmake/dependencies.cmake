9e7495965dbbe3dc46d635163255e1b9657ec4e2
Jack Jamison <jackqjamison@gmail.com>
2025-08-12 01:35:35 -0400

    cmake doesn't work

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,63 @@
+function(add_main_dependencies)
+
+	# CPPLocate (they have extreme cmake goofyness)
+	set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+	set(OPTION_BUILD_TESTS OFF) # these are cpplocate's tests
+	add_subdirectory("${EXTERNAL_DIR}/cpplocate" EXCLUDE_FROM_ALL)
+
+	# wasmtime
+	if (APPLE AND CONNECTION_MACHINE_DISTRIBUTE_APP)
+		add_library(wasmtime STATIC IMPORTED GLOBAL)
+		set_target_properties(wasmtime PROPERTIES
+			IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/lib-binary/libwasmtime.a"
+			INTERFACE_INCLUDE_DIRECTORIES "${EXTERNAL_DIR}/wasmtime/crates/c-api"  # where .h file is
+		)
+	else()
+		add_subdirectory("${EXTERNAL_DIR}/wasmtime/crates/c-api")
+	endif()
+
+	# JSON
+	set(CONNECTION_MACHINE_JSON_LIB_NAME "json")
+	add_library(${CONNECTION_MACHINE_JSON_LIB_NAME} INTERFACE)
+	target_include_directories(${CONNECTION_MACHINE_JSON_LIB_NAME} INTERFACE "${EXTERNAL_DIR}/json/")
+
+endfunction()
+
+function(add_app_dependencies)
+
+	# Find vulkan (we don't actually use the headers, we just want shaderc compiler (won't be needed when we switch to slang hopefully))
+	find_package(Vulkan REQUIRED COMPONENTS glslc)
+
+	# Volk vulkan meta loader
+	add_subdirectory("${EXTERNAL_DIR}/volk" SYSTEM EXCLUDE_FROM_ALL)
+
+	# SDL
+	if (CONNECTION_MACHINE_BUILD_TESTS) # hack to allow SDL to build without window system on linux
+		set(SDL_UNIX_CONSOLE_BUILD ON)
+	endif()
+	set(SDL_STATIC ON)
+	add_subdirectory("${EXTERNAL_DIR}/SDL" SYSTEM EXCLUDE_FROM_ALL)
+
+	# STB Image
+	set(CONNECTION_MACHINE_STB_IMAGE_LIB_NAME "stb_image")
+	add_library(${CONNECTION_MACHINE_STB_IMAGE_LIB_NAME} INTERFACE)
+	target_include_directories(${CONNECTION_MACHINE_STB_IMAGE_LIB_NAME} INTERFACE "${EXTERNAL_DIR}/stb/")
+	
+	# Freetype
+	add_subdirectory("${EXTERNAL_DIR}/freetype" SYSTEM EXCLUDE_FROM_ALL)
+	add_library(Freetype::Freetype ALIAS freetype)
+
+	# RmlUi
+	set(RMLUI_BACKEND native)
+	add_subdirectory("${EXTERNAL_DIR}/RmlUi" SYSTEM EXCLUDE_FROM_ALL)
+
+	# VMA
+	add_subdirectory("${EXTERNAL_DIR}/VulkanMemoryAllocator" SYSTEM EXCLUDE_FROM_ALL)
+
+	# Vk Bootstrap
+	add_subdirectory("${EXTERNAL_DIR}/vk-bootstrap" SYSTEM EXCLUDE_FROM_ALL)
+
+	# GLM
+	add_subdirectory("${EXTERNAL_DIR}/glm" SYSTEM EXCLUDE_FROM_ALL)
+
+endfunction()
