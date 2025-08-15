40a18ed2a44f17db4e3b08a36736c713f5f51296
ItchyTrack <benherman345@gmail.com>
2025-08-15 11:54:52 -0700

    fixed CID:1619965

--- src/util/algorithm.h
+++ src/util/algorithm.h
@@ -12,18 +12,14 @@ inline bool contains(Iterator firstIter, Iterator lastIter, const T& value) {
 
 inline std::vector<std::string> stringSplit(const std::string& s, const char delimiter) {
 	size_t start = 0;
-	size_t end = s.find_first_of(delimiter);
+	size_t end = 0;
 
 	std::vector<std::string> output;
 
-	while (end <= std::string::npos) {
-		output.emplace_back(s.substr(start, end - start));
-
-		if (end == std::string::npos)
-			break;
-
-		start = end + 1;
+	while (end != std::string::npos) {
 		end = s.find_first_of(delimiter, start);
+		output.emplace_back(s.substr(start, end - start));
+		start = end + 1;
 	}
 
 	return output;
@@ -31,16 +27,11 @@ inline std::vector<std::string> stringSplit(const std::string& s, const char del
 
 inline void stringSplitInto(const std::string& s, const char delimiter, std::vector<std::string>& vectorToFill) {
 	size_t start = 0;
-	size_t end = s.find_first_of(delimiter);
-
-	while (end <= std::string::npos) {
-		vectorToFill.emplace_back(s.substr(start, end - start));
-
-		if (end == std::string::npos)
-			break;
-
-		start = end + 1;
+	size_t end = 0;
+	while (end != std::string::npos) {
 		end = s.find_first_of(delimiter, start);
+		vectorToFill.emplace_back(s.substr(start, end - start));
+		start = end + 1;
 	}
 }
 
