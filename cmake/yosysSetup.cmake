b0151a2687ea1ef978b32aba13a13bff591cb4e3
ItchyTrack <benherman345@gmail.com>
2025-08-22 13:39:39 -0700

    its linking correctly!!!

--- cmake/yosysSetup.cmake
+++ cmake/yosysSetup.cmake
@@ -6,6 +6,14 @@ CPMAddPackage(
 	SOURCE_DIR "${EXTERNAL_DIR}/yosys"
 )
 
+# link yosys-slang to yosys to have it loaded as a plugin
+if(MSVC)
+    set(YOSYS_LIB_SUFFIX ".dll")
+else()
+    set(YOSYS_LIB_SUFFIX ".so")
+endif()
+set(YOSYS_BIN_LOCATION "${yosys_BINARY_DIR}/libyosys${YOSYS_LIB_SUFFIX}")
+
 file(WRITE "${CMAKE_BINARY_DIR}/cmake/FindYosys.cmake"
 	"include(CMakeFindDependencyMacro)
 	add_library(yosys::yosys INTERFACE IMPORTED)
@@ -28,45 +36,82 @@ CPMAddPackage(
 		"BUILD_AS_PLUGIN OFF"
 	SOURCE_DIR "${EXTERNAL_DIR}/yosys-slang"
 )
-list(APPEND EXTERNAL_LINKS yosys-slang)
+set(YOSYS_SLANG_BIN_LOCATION "${CMAKE_BINARY_DIR}/libyosys-slang.a")
+# list(APPEND EXTERNAL_LINKS yosys-slang)
 
-# link yosys-slang to yosys to have it loaded as a plugin
-cmake_policy(SET CMP0079 NEW)
-
-if(APPLE)
-	set(yosys_slang_link_command "-Wl,-force_load,$<TARGET_FILE:yosys-slang>")
+if(CMAKE_C_COMPILER_ID STREQUAL "Clang" OR CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
+    set(YOSYS_CONFIG_COMPILER "clang")
+elseif(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+    set(YOSYS_CONFIG_COMPILER "gcc")
+elseif(MSVC)
+    set(YOSYS_CONFIG_COMPILER "none")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows" AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(YOSYS_CONFIG_COMPILER "msys2-64")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows" AND CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set(YOSYS_CONFIG_COMPILER "msys2-32")
 else()
-	set(yosys_slang_link_command "-Wl,--push-state,--whole-archive $<TARGET_FILE:yosys-slang> -Wl,--pop-state")
+    set(YOSYS_CONFIG_COMPILER "none")
+endif()
+
+if(MSVC)
+    set(YOSYS_LINK_FLAGS "")
+else()
+	if(APPLE)
+		set(YOSYS_LINK_FLAGS "-Wl,-force_load,${YOSYS_SLANG_BIN_LOCATION}")
+	else()
+		set(YOSYS_LINK_FLAGS "-Wl,--push-state,--whole-archive ${YOSYS_SLANG_BIN_LOCATION} -Wl,--pop-state")
+	endif()
 endif()
 
 # build yosys
+file(WRITE "${yosys_BINARY_DIR}/Makefile.conf" "CONFIG := ${YOSYS_CONFIG_COMPILER}\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "LINKFLAGS += ${YOSYS_LINK_FLAGS}\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_TCL := 0\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_ABC := 0\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_GLOB := 1\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_PLUGINS := 1\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_READLINE := 0\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_VERIFIC_SYSTEMVERILOG := 0\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_VERIFIC_EDIF := 0\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_VERIFIC_LIBERTY := 0\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_COVER := 1\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_LIBYOSYS := 1\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "DISABLE_SPAWN := 1\n")
+file(APPEND "${yosys_BINARY_DIR}/Makefile.conf" "ENABLE_ZLIB := 1\n")
+
+find_program(MAKE_EXE make)
+if(NOT MAKE_EXE)
+	if (WIN32)
+		message(FATAL_ERROR "GNU Make not found. On Windows, install MSYS2 or MinGW and ensure 'make' is in PATH.")
+	elseif()
+		message(FATAL_ERROR "Make not found! ensure 'make' is in PATH.")
+	endif()
+endif()
+
 execute_process(
-    COMMAND ${CMAKE_COMMAND} -E echo "CONFIG := clang" > ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "LINKFLAGS += ${yosys_slang_link_command}" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_TCL := 0" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_ABC := 0" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_GLOB := 1" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_PLUGINS := 1" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_READLINE := 0" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_VERIFIC_SYSTEMVERILOG := 0" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_VERIFIC_EDIF := 0" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_VERIFIC_LIBERTY := 0" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_COVER := 1" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_LIBYOSYS := 1" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "DISABLE_SPAWN := 1" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-    COMMAND ${CMAKE_COMMAND} -E echo "ENABLE_ZLIB := 1" >> ${EXTERNAL_DIR}/yosys/Makefile.conf
-	COMMAND make share
+	COMMAND ${MAKE_EXE} -f "${EXTERNAL_DIR}/yosys/Makefile" share
     WORKING_DIRECTORY ${EXTERNAL_DIR}/yosys
 )
 
-add_custom_target(build_yosys ALL
-    BYPRODUCTS "${EXTERNAL_DIR}/yosys/libyosys.so"
-    COMMAND make -j8 libyosys.so
-    COMMAND ${CMAKE_INSTALL_NAME_TOOL} -id "${EXTERNAL_DIR}/yosys/libyosys.so" "${EXTERNAL_DIR}/yosys/libyosys.so"
-    WORKING_DIRECTORY ${EXTERNAL_DIR}/yosys
-	COMMENT "Building Yosys..."
-	USES_TERMINAL
-)
+if (APPLE)
+	add_custom_target(build_yosys ALL
+		BYPRODUCTS ${YOSYS_BIN_LOCATION}
+		COMMAND ${MAKE_EXE} -f ${EXTERNAL_DIR}/yosys/Makefile -j8 libyosys${YOSYS_LIB_SUFFIX}
+		COMMAND ${CMAKE_INSTALL_NAME_TOOL} -id ${YOSYS_BIN_LOCATION} ${YOSYS_BIN_LOCATION}
+		WORKING_DIRECTORY ${yosys_BINARY_DIR}
+		COMMENT "Building Yosys..."
+		USES_TERMINAL
+	)
+else()
+	add_custom_target(build_yosys ALL
+		BYPRODUCTS ${YOSYS_BIN_LOCATION}
+		COMMAND ${MAKE_EXE} -f ${EXTERNAL_DIR}/yosys/Makefile -j8 libyosys${YOSYS_LIB_SUFFIX}
+		WORKING_DIRECTORY ${yosys_BINARY_DIR}
+		COMMENT "Building Yosys..."
+		USES_TERMINAL
+	)
+endif()
+add_dependencies(build_yosys yosys-slang)
 
 # link it together
 add_library(Yosys SHARED IMPORTED)
@@ -74,7 +119,7 @@ add_dependencies(Yosys build_yosys)
 add_compile_definitions(YOSYS_DISABLE_SPAWN)
 add_compile_definitions(_YOSYS_)
 set_target_properties(Yosys PROPERTIES
-	IMPORTED_LOCATION "${EXTERNAL_DIR}/yosys/libyosys.so"
+	IMPORTED_LOCATION ${YOSYS_BIN_LOCATION}
 	INTERFACE_INCLUDE_DIRECTORIES ${EXTERNAL_DIR}/yosys/share/include
 )
 list(APPEND EXTERNAL_LINKS Yosys)
